c4f343b7e547b3cb78107cf54429cd187b25cd6c
kate <81867634+kakovkinak@users.noreply.github.com>
2023-12-04 16:00:22 +0000

    init

--- src/components/ProductItem/ProductItem.jsx
+++ src/components/ProductItem/ProductItem.jsx
@@ -9,7 +9,7 @@ const ProductItem = ({product, className, onAdd}) => {
 
     return (
         <div className={'product ' + className}>
-            <div className={'img'}>{product.img}</div>
+            <div className={'img'}> <img src="img"/></div>
             <div className={'title'}>{product.title}</div>
             <div className={'description'}>{product.description}</div>
             <div className={'price'}>
