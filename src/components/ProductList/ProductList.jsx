6ee52c69a6701b1b9fab8db3a88a0045be90781e
kate <81867634+kakovkinak@users.noreply.github.com>
2023-12-04 14:21:43 +0000

    init

--- src/components/ProductList/ProductList.jsx
+++ src/components/ProductList/ProductList.jsx
@@ -1,15 +1,15 @@
-import React, {useState} from 'react';
+import React, {useCallback, useEffect, useState} from 'react';
 import './ProductList.css';
 import ProductItem from "../ProductItem/ProductItem";
 import {useTelegram} from "../../hook/useTelegram";
 
 const products = [
-    {id: '1', title: 'Кеди', price: 1549, description: 'Білого кольору'},
-    {id: '2', title: 'Туфлі', price: 4800, description: 'Бежевого кольору'},
-    {id: '3', title: 'Кросівки', price: 2100, description: 'Чорного кольору'},
-    {id: '4', title: 'Сланці', price: 990, description: 'Чорного кольору'},
-    {id: '5', title: 'Черевики', price: 5000, description: 'Бежевого кольору'},
-    {id: '6', title: 'Мокасини', price: 1915, description: 'Коричневого кольору'},
+    {id: '1', img: 'img/Коверс_б.jpeg', title: 'Кеди', price: 1549, description: 'Білого кольору'},
+    {id: '2', img: 'img/Туфлі_бе.jpeg', title: 'Туфлі', price: 4800, description: 'Бежевого кольору'},
+    {id: '3', img: 'img/Кросівки_ч.jpeg',title: 'Кросівки', price: 2100, description: 'Чорного кольору'},
+    {id: '4', img: 'img/Сланці_Ч.jpeg',title: 'Сланці', price: 990, description: 'Чорного кольору'},
+    {id: '5', img: 'img/Черевики_беж.jpeg',title: 'Черевики', price: 5000, description: 'Бежевого кольору'},
+    {id: '6', img: 'img/Мокасини_кор.jpeg',title: 'Мокасини', price: 1915, description: 'Коричневого кольору'},
 ]
 
 const getTotalPrice = (items = []) => {
@@ -21,6 +21,26 @@ const getTotalPrice = (items = []) => {
 const ProductList = () => {
     const [addedItems, setAddedItems] = useState([]);
     const {tg} = useTelegram();
+    const onSendData = useCallback(() => {
+        const data = {
+          products: addedItems,
+            totalPrice: getTotalPrice(addedItems),
+        }
+        fetch('http://localhost:8080', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(data)
+        })
+    }, [])
+
+    useEffect(() => {
+        tg.onEvent('mainButtonClicked', onSendData)
+        return () => {
+            tg.offEvent('mainButtonClicked', onSendData)
+        }
+    }, [onSendData])
     const onAdd = (product) => {
         const alreadyAdded = addedItems.find(item => item.id === product.id);
         let newItems = [];
